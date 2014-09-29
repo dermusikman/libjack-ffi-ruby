9935f8cf19c319531d528a921c02c62a75c7c33f
dermusikman <jhadley@intoxitrack.net>
2014-09-28 20:04:06 -0500

    Clean up jack.rb

--- lib/jack.rb
+++ lib/jack.rb
@@ -1,22 +1,3 @@
-=begin
-    libjack-ffi-ruby - JACK bindings for ruby via FFI interface
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
-    Author: Marcin Lewandowski <marcin at saepia dot net>
-=end    
-    
 require 'ffi'
 
 module FFI
@@ -29,7 +10,7 @@ module FFI
         last = tmp.send(reader)
         break if last.null?
         ary << last
-        tmp += size 
+        tmp += size
       end
       ary
     end
@@ -45,7 +26,7 @@ module FFI
 end
 
 module JACK
-  LIB = [ "libjack.so.0.0.28", "libjack.so.0", "libjack.so", "libjack" ]  
+  LIB = [ "libjack.so.0.0.28", "libjack.so.0", "libjack.so", "libjack" ]
   VERSION = "0.0.1"
 end
 
